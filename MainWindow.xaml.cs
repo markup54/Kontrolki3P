2ced4bcaf8420ea6dd10a76ef75dad4afcc5c3b6
MarzenaK <marzenakupka@tlen.pl>
2021-12-02 12:56:18 +0100

    bindowanie

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,46 @@ namespace Kontrolki3P
         {
             InitializeComponent();
         }
+
+        private void bok_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            double boczek;
+            if(double.TryParse(bok.Text,out boczek) && boczek>0)
+            {
+                obwod.Text = (boczek * 4).ToString();
+                pole.Text = Math.Pow(boczek, 2).ToString();
+                komunikat.Content = String.Empty;
+
+            }
+            else
+            {
+                komunikat.Content = "Podaj wartość dodatnią";
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            bok.Text = String.Empty;
+            pole.Text = String.Empty;
+            obwod.Text = String.Empty;
+            komunikat.Content = "Wpisz wymiar boku";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            double boczek;
+            if (double.TryParse(bok.Text, out boczek))
+            {
+                kwadrat.Width = boczek;
+                kwadrat.Height = boczek;
+                SolidColorBrush kolorek 
+                    = (SolidColorBrush)new BrushConverter()
+                    .ConvertFromString(kolorCb.Text);
+                kwadrat.Fill = kolorek;
+                kwadrat.Stroke = kolorek;
+                kwadrat.Opacity = (przezChk.IsChecked.Value) ? 0.5 : 1;
+
+            }
+        }
     }
 }
